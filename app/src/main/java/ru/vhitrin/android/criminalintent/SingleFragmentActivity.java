dedd2f461a84bafd3756e178027fc33f92ae4ee5
Хитрин Влад <v.hitrin@gmail.com>
2017-02-26 00:00:40 +0300

    remove unused import

--- app/src/main/java/ru/vhitrin/android/criminalintent/SingleFragmentActivity.java
+++ app/src/main/java/ru/vhitrin/android/criminalintent/SingleFragmentActivity.java
@@ -2,7 +2,6 @@ package ru.vhitrin.android.criminalintent;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 
